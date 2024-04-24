14c48646d8e6be3d6e230586270c2c233bdd969b
Francis lalaguna <franc@UBVPNN7599.core.unionbank.com>
2024-04-24 10:14:11 +0800

    update officialMember fetching server data

--- 42-lions-road/src/components/OfficialMembers.js
+++ 42-lions-road/src/components/OfficialMembers.js
@@ -1,16 +1,16 @@
 import '../styles/Cards.css'
-const OfficialMember = ({ image, date, title, info, location, duration, cost }) => {
-  // const OfficialMember = ({ filename, name, info, title}) => {
+// const OfficialMember = ({ image, date, title, info, location, duration, cost }) => {
+  const OfficialMember = ({ filename, name, info, title}) => {
   return (
     <article className='tour-card'>
       <div className='tour-img-container'>
-        {/* <img src={`http://ec2-54.95.255.71.ap-northeast-1.compute.amazonaws.com:3000/images/`+ filename} className='tour-img' alt={title} /> */}
-        <img src={image} className='tour-img' alt={title} />
+        <img src={`http://ec2-13-231-242-0.ap-northeast-1.compute.amazonaws.com:3001/images/`+ filename} className='tour-img' alt={title} />
+        {/* <img src={image} className='tour-img' alt={title} /> */}
         <p className='tour-date'>{title}</p>
       </div>
       <div className='tour-info'>
         <div className='tour-title'>
-          <h4>{date}</h4>
+          <h4>{name}</h4>
         </div>
         <p>{info}</p>
         <div className='tour-footer'>
